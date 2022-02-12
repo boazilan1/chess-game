1ebc835eb7ba9092ae2c7bf8a0bb3ba7292feb49
boaz ilan <boazila1@gamil.com>
2022-02-13 01:30:30 +0200

    2boli

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express =require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const app =express();
 
-app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.set('views', path.join(__dirname, './views'));
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 const boaz="boaz";
 app.get("/", function (req, res) {
 
