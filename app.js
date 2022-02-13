ae84658a51a27f3344623c60af0688fe391eaf80
boaz ilan <boazila1@gamil.com>
2022-02-13 02:06:54 +0200

    2boli

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get("/", function (req, res) {
 
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000,  () =>{
     console.log("Server started on ");
   });
   
